e1a8649d61ac10f77510d342b93c9fc98d393563
Samuel J. Mancebo <sjmancebo@gmail.com>
2021-08-17 11:27:53 -0400

    testing vercel.json config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './screens/home';
 
 function App() {
